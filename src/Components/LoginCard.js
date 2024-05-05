b5ae0d0a8f2074c48242e3bf06f36ab570a59fe6
nournaderr <133047680+nournaderr@users.noreply.github.com>
2024-05-06 01:26:09 +0300

    Update LoginCard.js

--- src/Components/LoginCard.js
+++ src/Components/LoginCard.js
@@ -9,35 +9,35 @@ export default function LoginCard() {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const handleLogin = () => {
-    const userData = {
-      username: username,
-      password: password,
-    };
-    const url = "https://collabbackend.onrender.com/login";
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        // Optionally, include any additional headers as needed
-      },
-      body: JSON.stringify(userData),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Login failed");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        // Handle successful login response
-        console.log("Login successful:", data);
-        navigate("/TextEditor"); // Navigate to documents page upon successful login
-      })
-      .catch((error) => {
-        // Handle error
-        navigate("/register"); // Navigate to documents page upon successful login
-        console.error("Error logging in:", error);
-      });
+    // const userData = {
+    //   username: username,
+    //   password: password,
+    // };
+    // const url = "https://collabbackend.onrender.com/login";
+    // fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     // Optionally, include any additional headers as needed
+    //   },
+    //   body: JSON.stringify(userData),
+    // })
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error("Login failed");
+    //     }
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     // Handle successful login response
+    //     console.log("Login successful:", data);
+    navigate("/TextEditor"); // Navigate to documents page upon successful login
+    //   })
+    //   .catch((error) => {
+    //     // Handle error
+    //     navigate("/register"); // Navigate to documents page upon successful login
+    //     console.error("Error logging in:", error);
+    //   });
   };
   //     try {
   //       const response = await axios.post(
