ee3e29b04236558ee880cc2428082b3c9995d0d2
nournaderr <133047680+nournaderr@users.noreply.github.com>
2024-05-14 20:55:34 +0300

    Update TextEditor.js

--- src/Pages/TextEditor.js
+++ src/Pages/TextEditor.js
@@ -50,7 +50,7 @@ const TextEditor = ({ value, onChange }) => {
   }, [buffer]);
 
   const handleSendMessage = (character, index) => {
-    if (stompClientRef.current !== null) {
+    if (stompClientRef.current !== null && sessionID !== null) {
       const operation = 0;
       stompClientRef.current.send(
         `/app/application/${docID}`,
